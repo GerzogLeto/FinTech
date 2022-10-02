9597b25339e009ae0e562f738576f8913399aace
Stepfano <stepfano@yandex.ru>
2022-10-02 08:52:24 +0300

    Add processing sum transaction for concrete month (january)
    For transactions from csv files

--- src/main/java/BankTransactionAnalyzerSimple.java
+++ src/main/java/BankTransactionAnalyzerSimple.java
@@ -2,20 +2,27 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class BankTransactionAnalyzerSimple {
-    private static final String RESOURCES = "./transactions/";
+    private static final String RESOURCES = "./transactions/transactions.csv";
 
     public static void main(String[] args) throws IOException {
-        Path path = Paths.get(RESOURCES + args[0]);
+        Path path = Paths.get(RESOURCES);
         List<String> lines = Files.readAllLines(path);
         double total = 0d;
+        final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
         for (String line : lines) {
             final String[] columns = line.split(",");
-            final double amount = Double.parseDouble(columns[1]);
-            total += amount;
+            final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
+            if (date.getMonth() == Month.JANUARY) {
+                final double amount = Double.parseDouble(columns[1]);
+                total += amount;
+            }
         }
-        System.out.println("The total for all transactions is " + total);
+        System.out.println("The total for all transactions in January is " + total);
     }
 }
